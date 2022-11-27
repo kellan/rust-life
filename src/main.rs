55524b99cd3d7b959b911fabf0a76930aa9c8835
kellan <kellan@pobox.com>
2022-11-27 11:28:16 -0500

    vec of vec of cell enums

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use std::collections::HashMap;
-
 const DIRECTIONS: [(i8, i8); 8] = [
     (-1, -1),
     (-1, 0),
@@ -13,72 +11,69 @@ const DIRECTIONS: [(i8, i8); 8] = [
 
 #[derive(Debug)]
 struct Board {
-    height: i8,
-    width: i8,
-    map: HashMap<Point, Cell>,
+    height: u8,
+    width: u8,
+    map: Vec<Vec<Cell>>,
 }
 
-#[derive(Debug)]
-struct Cell {
-    alive: bool,
-    neighbor_cnt: i8,
-}
-
-impl Cell {
-    fn new() -> Cell {
-        Cell { neighbor_cnt: 0 }
-    }
+#[derive(Clone, Debug)]
+enum Cell {
+    Dead,
+    Live(i8),
 }
 
 #[derive(PartialEq, Eq, Hash, Debug)]
 struct Point(i8, i8);
 
 impl Board {
-    fn new(height: i8, width: i8) -> Board {
+    fn new(height: u8, width: u8) -> Board {
         let board = Board {
             height: height,
             width: width,
-            map: HashMap::new(),
+            map: vec![vec![Cell::Dead; usize::from(width)]; usize::from(height)],
         };
 
         board
     }
 
-    fn insert_cell(&mut self, hw: Point) {
-        self.map.insert(hw, Cell::new());
-    }
+    // fn insert_cell(&mut self, hw: Point) {
+    //     self.map.insert(hw, Cell::new());
+    // }
 
-    fn living_neighbors(&self, hw: Point) -> Vec<&Cell> {
-        let points = self.neighbor_points(hw);
-        let mut cells = Vec::<&Cell>::new();
-        for p in points {
-            if let Some(cell) = self.map.get(&p) {
-                cells.push(cell);
-            }
-        }
-
-        cells
-    }
-
-    // fn neighbor_cells(&self, hw: Point) -> Vec<&Cell> {
+    // fn living_neighbors(&self, hw: Point) -> Vec<&Cell> {
     //     let points = self.neighbor_points(hw);
     //     let mut cells = Vec::<&Cell>::new();
     //     for p in points {
-    //         if self.map.contains_key(&p) {
-    //             cells.push(self.map.get(&p).unwrap());
+    //         if let Some(cell) = self.map.get(&p) {
+    //             cells.push(cell);
     //         }
     //     }
 
     //     cells
     // }
 
+    // // fn neighbor_cells(&self, hw: Point) -> Vec<&Cell> {
+    // //     let points = self.neighbor_points(hw);
+    // //     let mut cells = Vec::<&Cell>::new();
+    // //     for p in points {
+    // //         if self.map.contains_key(&p) {
+    // //             cells.push(self.map.get(&p).unwrap());
+    // //         }
+    // //     }
+
+    // //     cells
+    // // }
+
     fn neighbor_points(&self, hw: Point) -> Vec<Point> {
         let mut neighbors = Vec::<Point>::new();
+        let height = self.height as i8;
+        let width = self.width as i8;
+
         for delta in DIRECTIONS {
             let dh = hw.0 - delta.0;
             let dw = hw.1 - delta.1;
 
-            if (dh >= 0 && dh < self.height) && (dw >= 0 && dw < self.width) {
+            if (dh >= 0 && dh < height) && (dw >= 0 && dw < width) {
                 neighbors.push(Point(dh, dw));
             }
         }
@@ -113,15 +108,15 @@ mod tests {
         assert!(!cells.contains(&Point(1, 2)));
     }
 
-    #[test]
-    fn cell_tests() {
-        let mut board = Board::new(3, 3);
-        let living = board.living_neighbors(Point(0, 0));
-        assert_eq!(living.len(), 0);
-        board.insert_cell(Point(0, 1));
-        let cell = board.map.get(&Point(0, 1));
-        assert!(cell.is_some());
-        let living = board.living_neighbors(Point(0, 0));
-        assert_eq!(living.len(), 1);
-    }
+    // #[test]
+    // fn cell_tests() {
+    //     let mut board = Board::new(3, 3);
+    //     let living = board.living_neighbors(Point(0, 0));
+    //     assert_eq!(living.len(), 0);
+    //     board.insert_cell(Point(0, 1));
+    //     let cell = board.map.get(&Point(0, 1));
+    //     assert!(cell.is_some());
+    //     let living = board.living_neighbors(Point(0, 0));
+    //     assert_eq!(living.len(), 1);
+    // }
 }
